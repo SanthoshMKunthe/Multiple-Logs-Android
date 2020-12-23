3f25b362d91b02b39189e20c1582c643079b71f1
Santhosh M K <santhoshmkunthe@gmail.com>
2020-12-23 16:26:26 +0530

    Added 10 types

---
+++ classifiedlogins/src/main/java/com/excelsoftcorp/classifiedlogins/smkl.java
@@ -0,0 +1,47 @@
+package com.excelsoftcorp.classifiedlogins;
+
+import android.util.Log;
+
+public class smkl {
+
+    public static void a(String logString) {
+        Log.d("TypeA", logString);
+    }
+
+    public static void b(String logString) {
+        Log.d("TypeB", logString);
+    }
+
+    public static void c(String logString) {
+        Log.d("TypeC", logString);
+    }
+
+    public static void d(String logString) {
+        Log.d("TypeD", logString);
+    }
+
+    public static void e(String logString) {
+        Log.d("TypeE", logString);
+    }
+
+    public static void f(String logString) {
+        Log.d("TypeF", logString);
+    }
+
+    public static void g(String logString) {
+        Log.d("TypeG", logString);
+    }
+
+    public static void h(String logString) {
+        Log.d("TypeH", logString);
+    }
+
+    public static void i(String logString) {
+        Log.d("TypeI", logString);
+    }
+
+    public static void j(String logString) {
+        Log.d("TypeJ", logString);
+    }
+
+}
