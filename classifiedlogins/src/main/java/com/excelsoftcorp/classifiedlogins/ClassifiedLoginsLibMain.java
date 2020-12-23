98e2f1aa012292a75019538316c436d6094902d6
Santhosh M K <santhosh.mk@excelindia.com>
2020-12-23 14:12:10 +0530

    Classified Logins with 10 types

---
+++ classifiedlogins/src/main/java/com/excelsoftcorp/classifiedlogins/ClassifiedLoginsLibMain.java
@@ -0,0 +1,10 @@
+package com.excelsoftcorp.classifiedlogins;
+
+import android.util.Log;
+
+public class ClassifiedLoginsLibMain {
+
+    public static void TypeA(String logString) {
+        Log.d("TypeA", logString);
+    }
+}
